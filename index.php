6c6898049188f24bf1cc61051940f298075edbbb
Francesca Barletta <wallebar@gmail.com>
2024-05-09 15:12:24 +0200

    agg class production and 3 istances

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+class Production
+{
+    public $title;
+    public $language;
+    public $vote;
+    function __construct($_title, $_language, $_vote )
+    {
+        $this->title = $_title;
+        $this->language = $_language;
+        $this->setvote($_vote);
+
+    }
+
+    public function setvote($vote)
+    {
+    
+    if(is_numeric($vote) && $vote <= 10 && $vote >= 1){
+        $this->vote = intval($vote);
+    } else {
+        var_dump('valore accettato solo da 1 a 10');
+    }
+    }
+}
+
+
+
+$movie1 = new Production('Ritorno al futuro', 'Inglese', 9);
+$movie2 = new Production('Titanic', 'Inglese', 8);
+$movie3 = new Production('Interstellar','inglese', 6.5);
+
+var_dump($movie1, $movie2, $movie3);
